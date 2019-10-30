8f46bd0a63a2c53657ace1caba1c1e586bb7270c
erisikhwan <erisvino17@gmail.com>
2019-10-30 17:24:13 +0700

    -UTS-MODUL-DASAR-PEMROGRAMAN

---
+++ Bab 2/2_deklarasi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int jumlahUang;
+
+    jumlahUang = 1000;
+    cout << "JUmalah uang semula: " << jumlahUang << endl;
+
+    // Varuabel diubah
+
+    jumlahUang = 1200;
+    cout << "Jumlah uang sekarang: " << jumlahUang << endl;
+
+    return 0;
+}
